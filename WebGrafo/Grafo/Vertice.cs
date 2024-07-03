5034eea743d38b715d973b6edb77c66485712500
Jorge <jlimonmunguia@gmail.com>
2024-07-03 02:49:57 -0600

    Añaniendo Clase Vertice

---
+++ WebGrafo/Grafo/Vertice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafo
+{
+    public class Vertice
+    {
+        public Ciudad city = null;
+        public ListaArista ListaEnlaces = new ListaArista();
+
+        public Vertice(Ciudad datos)
+        {
+            city = datos;
+        }
+
+        public string AgregarLista(int numv,float distancia)
+        {
+            return ListaEnlaces.Insertar(numv, distancia);
+        }
+
+        public string[] MuestraAristas()
+        {
+            return ListaEnlaces.MostrarColeccion();
+        }
+
+        public int[] ObtenerPos()
+        {
+            return ListaEnlaces.ObtenerPosiciones();
+        }
+
+        public float ObtenerCosto(int posAr)
+        {
+            return ListaEnlaces.RetornarPeso(posAr);
+        }
+        public string BorrarLista(int numVD)
+        {
+            return ListaEnlaces.Borrar(numVD);
+        }
+
+        public string infoCiudad()
+        {
+            return $"{city.nomciudad}";
+        }
+    }
+}
