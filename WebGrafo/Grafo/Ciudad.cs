6dd8fb0a3733f04fed61c22ae04f742bb9e21cb4
Jorge <jlimonmunguia@gmail.com>
2024-07-03 02:49:00 -0600

    Añaniendo Clase Ciudad

---
+++ WebGrafo/Grafo/Ciudad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafo
+{
+    public class Ciudad
+    {
+        //Esta clase, será la que quede almacenada en el vertice
+        public string nomciudad { get; set; }
+        public int totalhab { get; set; }
+        public float superficiekm { get; set; }
+
+        public Ciudad() { }
+
+        public Ciudad(string nomc, int totlhab, float sup)
+        {
+            nomciudad = nomc;
+            totalhab = totlhab;
+            superficiekm = sup;
+        }
+
+    }
+}
